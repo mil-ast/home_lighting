52970b95ec886e7b997a6cd4de3c8bd330b6c207
Kochetov Pavel <mil-ast@yandex.ru>
2022-12-01 21:37:53 +0300

    доводка

--- home_lighting.ino
+++ home_lighting.ino
@@ -1,13 +1,14 @@
 #define IS_DEBUG_MODE 0
-#define CORRIDOR_LIGHT_ON_DELAY 8000
+#define CORRIDOR_LIGHT_ON_DELAY 6000
+#define STAIRS_LIGHT_ON_DELAY 6000
 
 enum State {
+  STATE_WAIT,
   STATE_UP,
   STATE_DOWN,
-  STATE_WAIT,
 };
 
-const int maxLightLevel = 300; // макс значение датчика света для работы (0 темно, 1023 светло)
+const int maxLightLevel = 130; // макс значение датчика света для работы (0 темно, 1023 светло)
 const int lightPin = A3; // датчик света
 const int pinSwitchCorridor = 5; // включатель коридора   PWM 5
 const int pinSwitchStairs = 6; // включатель лестницы     PWM 6
@@ -16,10 +17,21 @@ const int pinMotionSensorCorridor2 = 8; // датчик движения кор�
 const int pinMotionSensorStairs1 = 9; // датчик движения лестницы верх
 const int pinMotionSensorStairs2 = 10; // датчик движения лестницы низ
 
-int lightLevelCorridor = 0; // текущий уровень освещения коридора
-unsigned long corridorTimeOn = 0; // время включения света, чтобы отключить позжеё
-State corridorState;
+struct StateValues {  
+  int lightLevel;
+  unsigned long timeOn ;
+  State state;
+  int sensor1;
+  int sensor2;
+};
 
+// коридор второго этажа
+struct StateValues coridorValues = { 0, 0, STATE_WAIT, 0, 0 };
+
+// лестница на второй этаж
+struct StateValues stairsValues = { 0, 0, STATE_WAIT, 0, 0 };
+
+const int lightLevelStep = 3;
 unsigned long lastLogTime = 0;
 
 void setup() {
@@ -27,85 +39,155 @@ void setup() {
     Serial.begin(9600);
   #endif
 
-  corridorState = STATE_WAIT;
-
-  //pinMode(pinSwitchCorridor, OUTPUT);
-  //digitalWrite(pinSwitchCorridor, LOW);
   pinMode(pinSwitchCorridor, OUTPUT);
   analogWrite(pinSwitchCorridor, 0);
-
   pinMode(pinMotionSensorCorridor1, INPUT);
   pinMode(pinMotionSensorCorridor2, INPUT);
+
+  pinMode(pinSwitchStairs, OUTPUT);
+  analogWrite(pinSwitchStairs, 0);
+  pinMode(pinMotionSensorStairs1, INPUT);
+  pinMode(pinMotionSensorStairs2, INPUT);
 }
 
 void loop() {
   // если светло и свет отключен, то ожидаем
   const int lightSensorValue = analogRead(lightPin);
   if (lightSensorValue > maxLightLevel) {
-    if (corridorState == STATE_WAIT && lightLevelCorridor == 0) {
+    if (coridorValues.state == STATE_WAIT && stairsValues.state == STATE_WAIT && coridorValues.lightLevel == 0 && stairsValues.lightLevel == 0) {
       #if (IS_DEBUG_MODE == 1)
+        lastLogTime = millis();
         Serial.print("Sleep, lightSensorValue: ");
         Serial.print(lightSensorValue);
         Serial.print("\r\n");
       #endif
       
-      delay(1000);
+      delay(5000);
       return;
     }
-  }
-
-  const int valueMotionSensorCorridor1 = digitalRead(pinMotionSensorCorridor1);
-  const int valueMotionSensorCorridor2 = digitalRead(pinMotionSensorCorridor2);
-  const bool isMotionSensorCorridor = valueMotionSensorCorridor1 == HIGH || valueMotionSensorCorridor2 == HIGH;
-
-  if (isMotionSensorCorridor) {
-    corridorTimeOn = millis();
-    corridorState = STATE_UP;
   }
 
   #if (IS_DEBUG_MODE == 1)
     if (duration(lastLogTime) > 1000) {
       lastLogTime = millis();
-      
-      Serial.print("motionSensorCorridor1: ");
-      Serial.print(valueMotionSensorCorridor1);
-      Serial.print(", motionSensorCorridor2: ");
-      Serial.print(valueMotionSensorCorridor2);
-      Serial.print(", corridorState: ");
-      Serial.print(corridorState);
-      Serial.print(", lightLevelCorridor: ");
-      Serial.print(lightLevelCorridor);
-      Serial.print(", lightSensorValue: ");
+      Serial.print("LightValue: ");
       Serial.print(lightSensorValue);
+      
+      Serial.print(" Coridor: ");
+      Serial.print(coridorValues.lightLevel);
+      Serial.print(", ");      
+      Serial.print(coridorValues.sensor1);
+      Serial.print(", ");
+      Serial.print(coridorValues.sensor2);
+
+      Serial.print(" Stairs: ");
+      Serial.print(stairsValues.lightLevel);
+      Serial.print(", ");        
+      Serial.print(stairsValues.sensor1);
+      Serial.print(", ");
+      Serial.print(stairsValues.sensor2);
+            
       Serial.print("\r\n");
     }
   #endif
 
-  switch (corridorState) {
+  corridorHandler();
+  stairsHandler();
+
+  delay(30);
+}
+
+
+void corridorHandler() {
+  const int valueMotionSensor1 = digitalRead(pinMotionSensorCorridor1);
+  const int valueMotionSensor2 = digitalRead(pinMotionSensorCorridor2);
+  const bool isMotionSensor = valueMotionSensor1 == HIGH || valueMotionSensor2 == HIGH;
+
+  coridorValues.sensor1 = valueMotionSensor1;
+  coridorValues.sensor2 = valueMotionSensor2;
+
+  if (isMotionSensor) {
+    coridorValues.timeOn = millis();
+    coridorValues.state = STATE_UP;
+  }
+
+  switch (coridorValues.state) {
     case STATE_UP:
-      if (lightLevelCorridor < 0xFF) {
-        lightLevelCorridor++;
-        analogWrite(pinSwitchCorridor, lightLevelCorridor);
+      if (coridorValues.lightLevel < 0xFF) {
+        coridorValues.lightLevel = lightIncrement(coridorValues.lightLevel);
+        analogWrite(pinSwitchCorridor, coridorValues.lightLevel);
       } else {
-        corridorState = STATE_WAIT;
+        coridorValues.state = STATE_WAIT;
       }
     break;
     case STATE_DOWN:
-      if (lightLevelCorridor > 0) {
-        lightLevelCorridor--;
-        analogWrite(pinSwitchCorridor, lightLevelCorridor);
+      if (coridorValues.lightLevel > 0) {
+        coridorValues.lightLevel = lighDecrement(coridorValues.lightLevel);
+        analogWrite(pinSwitchCorridor, coridorValues.lightLevel);
       } else {
-        corridorState = STATE_WAIT;  
+        coridorValues.state = STATE_WAIT;  
       }
     break;
     default:
       // если свет включен более чем, начнем отключать его
-      if (lightLevelCorridor > 0 && duration(corridorTimeOn) > CORRIDOR_LIGHT_ON_DELAY) {
-        corridorState = STATE_DOWN;
+      if (coridorValues.lightLevel > 0 && duration(coridorValues.timeOn) > CORRIDOR_LIGHT_ON_DELAY) {
+        coridorValues.state = STATE_DOWN;
       }
   }
+}
 
-  delay(10);
+void stairsHandler() {
+  const int valueMotionSensor1 = digitalRead(pinMotionSensorStairs1);
+  const int valueMotionSensor2 = digitalRead(pinMotionSensorStairs2);
+  const bool isMotionSensor = valueMotionSensor1 == HIGH || valueMotionSensor2 == HIGH;
+
+  stairsValues.sensor1 = valueMotionSensor1;
+  stairsValues.sensor2 = valueMotionSensor2;
+
+  if (isMotionSensor) {
+    stairsValues.timeOn = millis();
+    stairsValues.state = STATE_UP;
+  }
+
+  switch (stairsValues.state) {
+    case STATE_UP:
+      if (stairsValues.lightLevel < 0xFF) {
+        stairsValues.lightLevel = lightIncrement(stairsValues.lightLevel);
+        analogWrite(pinSwitchStairs, stairsValues.lightLevel);
+      } else {
+        stairsValues.state = STATE_WAIT;
+      }
+    break;
+    case STATE_DOWN:
+      if (stairsValues.lightLevel > 0) {
+        stairsValues.lightLevel = lighDecrement(stairsValues.lightLevel);
+        analogWrite(pinSwitchStairs, stairsValues.lightLevel);
+      } else {
+        stairsValues.state = STATE_WAIT;  
+      }
+    break;
+    default:
+      // если свет включен более чем, начнем отключать его
+      if (stairsValues.lightLevel > 0 && duration(stairsValues.timeOn) > STAIRS_LIGHT_ON_DELAY) {
+        stairsValues.state = STATE_DOWN;
+      }
+  }
+}
+
+int lightIncrement(int currentValue) {
+  int newValue = currentValue + lightLevelStep;
+  if (newValue >= 0xFF ) {
+    return 0xFF;
+  }
+  return newValue;
+}
+
+int lighDecrement(int currentValue) {
+  int newValue = currentValue - lightLevelStep;
+  if (newValue <= 0) {
+    return 0;
+  }
+  return newValue;
 }
 
 unsigned long duration(unsigned long t) {
